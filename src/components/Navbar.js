279c141e3112c0227d7a191f7af188bd358a7f04
donut <edwardtdo@gmail.com>
2022-10-27 14:32:20 +1100

    implemented an animated burger menu. refactoring now.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,37 @@
 import React, { useState } from 'react';
 import "../styles.css";
-import logo from "../images/lc-logo.png";
+import logo from "../images/lc-logo.svg";
  
 export default function Navbar() {
-	const [showMenu, setShowMenu] = useState(false);
+  const [burgerClass, setBurgerClass] = useState("burger-bar unclicked");
+  const [menuClass, setMenuClass] = useState("menu hidden");
+  const [menuClicked, setMenuClicked] = useState(false);
 
-  const toggleButton = () => {
-    setShowMenu(!showMenu);
-  };
-
+  const updateMenu = (e) => {
+    e.preventDefault();
+    
+    if (!menuClicked) {
+      setBurgerClass("burger-bar clicked");
+      setMenuClass("menu visible");
+    } else {
+      setBurgerClass("burger-bar unclicked");
+      setMenuClass("menu hidden");
+    }
+    setMenuClicked(!menuClicked);
+  }
+  
 	return (
 		<div id="navbar">
       <div className='container'>
         <div id="logo-container">
           <img id="logo" src={logo} alt="lawconnect logo" />
         </div>
-        <a className='mobile-only' href="#" id="toggle-button" onClick={toggleButton}>
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
+        <a className='mobile-only burger-menu' href="#" id="burger-menu" onClick={ updateMenu }>
+          <span className={ burgerClass }></span>
+          <span className={ burgerClass }></span>
+          <span className={ burgerClass }></span>
         </a>
+        
         <div className='desktop-only' id='navbar-links'>
           <ul>
             <li><a href="#">For People</a></li>
@@ -31,6 +43,13 @@ export default function Navbar() {
           <a href="#">Login</a>
           <button>Create Free Account</button>
         </div>
+      </div>
+      <div className={` ${ menuClass } mobile-only `}>
+        <ul>
+          <li><a href="#">For People</a></li>
+          <li><a href="#">For Businesses</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
       </div>
     </div>
 	)
